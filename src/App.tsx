6849caff6a1b6d2e3eb2f5ed9c00dbd4f49d0ca8
Keerthana N <kk1106726@gmail.com>
2026-01-21 09:49:14 +0530

    Remove App component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,1 @@
-export default function App() {
-  return (
-    <div className="h-screen flex">
-      {/* Left Panel */}
-      <div className="w-1/3 border-r p-4">
-        <h2 className="font-bold text-lg">Blogs</h2>
-        <p>Blog list will come here</p>
-      </div>
 
-      {/* Right Panel */}
-      <div className="w-2/3 p-4">
-        <h2 className="font-bold text-lg">Blog Details</h2>
-        <p>Select a blog to view details</p>
-      </div>
-    </div>
-  );
-}
